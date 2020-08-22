05d319b377fb0b51bb700d41dca9335d8a093f9f
Tan Le <tanle@Tans-MacBook-Pro.local>
2020-08-22 18:27:05 -0400

    create MainActivity. create simple MainActivity launch test. config Manifest to start. new configuration.

--- app/src/test/java/com/example/getmycar/MainActivityTest.kt
+++ app/src/test/java/com/example/getmycar/MainActivityTest.kt
@@ -1,13 +1,19 @@
 package com.example.getmycar
 
+import android.os.Build
+import androidx.test.core.app.launchActivity
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
 
 @RunWith(AndroidJUnit4::class)
+@Config(sdk = [Build.VERSION_CODES.O_MR1])
 class MainActivityTest {
 
     @Test
-    fun doNothing(){
+    fun canLaunchMainActivity(){
+        val scenario = launchActivity<MainActivity>()
     }
+
 }
