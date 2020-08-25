649e33ec7a2811f9bfb162066ab3af2a31befffa
Tan Le <tanle@Tans-MacBook-Pro.local>
2020-08-24 22:38:17 -0400

    create recylcerView with adapter

--- app/src/main/java/com/example/getmycar/CarAdapter.kt
+++ app/src/main/java/com/example/getmycar/CarAdapter.kt
@@ -1,17 +1,16 @@
 package com.example.getmycar
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.recyclerview.widget.RecyclerView
 
 class CarAdapter : RecyclerView.Adapter<CarAdapter.CarViewHolder>(){
 
-    var carTypes = listOf<String>("Truck, Sedan, Motorcycle, Sports, Super Car, Van, Wagon")
+    var carTypes = listOf<String>("Truck", "Sedan", "Motorcycle", "Sports", "Super Car", "Van", "Wagon")
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CarViewHolder {
-        val carTypeButton = LayoutInf later.from(parent.context).inflate(R.layout.item_car_type, parent, false) as Button
+        val carTypeButton = LayoutInflater.from(parent.context).inflate(R.layout.item_car_type, parent, false) as Button
         return CarViewHolder(carTypeButton)
     }
 
@@ -20,11 +19,8 @@ class CarAdapter : RecyclerView.Adapter<CarAdapter.CarViewHolder>(){
     }
 
     override fun onBindViewHolder(holder: CarViewHolder, position: Int) {
+        holder.button.text = carTypes.get(position)
     }
 
-
-    class CarViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-
-    }
-
+    class CarViewHolder(val button: Button): RecyclerView.ViewHolder(button)
 }
